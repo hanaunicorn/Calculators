c8bc2a37a5da6af3f4d1ec9bcb042764402862ba
sunshiine <76451888+hanaunicorn@users.noreply.github.com>
2023-07-06 11:26:12 -0700

    Update Practice-CalculatePiInput.py
    It works now!
    It just doesn't send a rejection message

--- Practice-CalculatePiInput.py
+++ Practice-CalculatePiInput.py
@@ -1,38 +1,19 @@
 import math
 
-def calculate_pi(digits):
-    if digits <= 0:
-        print("Number of digits must be positive.")
-        return
+def calculate_pi(circumference, diameter):
+    pi = circumference / diameter
+    pi = round(pi, 13)  # Round the value of pi to 13 decimal places
+    return pi
 
-    # Set the precision for pi calculation
-    precision = digits + 2
+# Get user input
+circumference = float(input("Enter the circumference: "))
+diameter = float(input("Enter the diameter: "))
 
-    # Calculate pi with the desired number of digits
-    pi = str(round(math.pi, precision))
+# Calculate pi
+pi = calculate_pi(circumference, diameter)
 
-    # Print the calculated value of pi
-    print(f"Pi: {pi}")
+# Format the string without trailing zeros
+pi_str = format(pi, '.13f').rstrip('0')
 
-
-def calculate_circle():
-    circumference = float(input("Enter the circumference of the circle: "))
-    diameter = float(input("Enter the diameter of the circle: "))
-
-    # Calculate radius using the given diameter
-    radius = diameter / 2
-
-    # Check if diameter and circumference form a valid circle
-    if circumference <= 0 or diameter <= 0 or abs(circumference - math.pi * diameter) > 1e-6:
-        print("Invalid circle.")
-        return
-
-    # Print the calculated radius
-    print(f"Radius: {radius}")
-
-
-# Example usage
-calculate_circle()
-
-digits = int(input("Enter the number of digits for pi calculation: "))
-calculate_pi(digits)
+# Print the first 13 digits of pi
+print(f"The first 13 digits of pi (the last digit is rounded) are: {pi_str}")
