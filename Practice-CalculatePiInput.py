29cc22a3272345ce5e4d41badbbb34fbfaf994f1
sunshiine <76451888+hanaunicorn@users.noreply.github.com>
2023-07-06 17:47:24 -0700

    Update Practice-CalculatePiInput.py
    Nilakantha series

--- Practice-CalculatePiInput.py
+++ Practice-CalculatePiInput.py
@@ -1,18 +1,16 @@
 # Set up variables to calculate
 def calculate_pi(n):
-    pi = 0.0
+    pi = 3.0
     sign = 1
 
-    # Repeat the equation to find answer
-    for i in range(n):
-        term = sign * (1 / (2*i + 1))  # Calculate the current term of the series
+    # Repeat the equation to find the answer
+    for i in range(1, n+1):
+        term = sign * (4 / ((2*i) * (2*i+1) * (2*i+2)))  # Calculate the current term of the series
         pi += term  # Add the term to the total sum
         sign *= -1  # Alternate the sign for the next term
 
-    pi *= 4  # Multiply the sum by 4 to approximate pi
     return pi
 
 num_terms = 100000  # Number of terms to be used for the approximation
-exact_pi = calculate_pi(num_terms)  # Call the calculate_pi function to get the approximate value of pi
-print("Exact value of pi:", exact_pi)  # Print the approximate value of pi
-
+approximate_pi = calculate_pi(num_terms)  # Call the calculate_pi_nilakantha function
+print("Approximate value of pi using Nilakantha series:", approximate_pi)  # Print the approximate value of pi
