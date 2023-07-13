f27c4d3b2698be5955a6923ab7805d18dff489c0
sunshiine <76451888+hanaunicorn@users.noreply.github.com>
2023-07-13 15:16:40 -0700

    Remove IDX, update
    remove idx
    replace with new find smallest function

--- InputNumberSortSelection.py
+++ InputNumberSortSelection.py
@@ -1,22 +1,24 @@
-# Selection sort version of InputNumberSort.py
-def selection_sort(input_list):
-    for a in range(len(input_list)):
-        min_idx = idx
-        for j in range(idx + 1, len(input_list)):
-            if input_list[min_idx] > input_list[j]:
-                min_idx = j
-        # Swap the minimum value with the compared value
-        input_list[idx], input_list[min_idx] = input_list[min_idx], input_list[idx]
-'''
-Index isolates the smallest number, which is how Selection Sort works.
-It then stacks and sorts more numbers following it.
-'''
-input_str = input("Enter a list of numbers separated by spaces: ")
+def find_smallest(input_list): # Replaces idx to find smallest number
+    smallest = input_list[0] # Clean list to input more number into later
+    for num in input_list:
+        if num < smallest: # Starts with 1st number, tries to find something smaller
+            smallest = num
+    return smallest
 
-try: # Finalizes list, and prints
+def selection_sort(input_list):
+    sorted_list = []
+    while input_list:
+        smallest = find_smallest(input_list)
+        sorted_list.append(smallest) # Builds up the list by adding "smallest" numbers 
+        input_list.remove(smallest) # Removes the output list numbers repeat from the Input list
+    return sorted_list
+
+input_str = input("Enter a list of numbers separated by spaces: ") # Asks user to input numbers
+
+try: # Prints out the output
     input_list = list(map(int, input_str.split()))
-    selection_sort(input_list)
-    print("Sorted list:", input_list)
+    sorted_list = selection_sort(input_list)
+    print("Sorted list:", sorted_list)
+    
 except ValueError: # Error Message
     print("Invalid input. Please enter a valid list of numbers separated by spaces.")
-
