e4f189dc71bbd81cb1c9e3a5b18499e3f5045d00
sunshiine <76451888+hanaunicorn@users.noreply.github.com>
2023-07-13 14:54:13 -0700

    Update InputNumberSortSelection.py
    fix spacing

--- InputNumberSortSelection.py
+++ InputNumberSortSelection.py
@@ -11,11 +11,12 @@ def selection_sort(input_list):
 Index isolates the smallest number, which is how Selection Sort works.
 It then stacks and sorts more numbers following it.
 '''
-
 input_str = input("Enter a list of numbers separated by spaces: ")
+
 try: # Finalizes list, and prints
     input_list = list(map(int, input_str.split()))
     selection_sort(input_list)
     print("Sorted list:", input_list)
 except ValueError: # Error Message
     print("Invalid input. Please enter a valid list of numbers separated by spaces.")
+
