37bdb11bff98a924dfd1b679dd8ea865c5b9a659
Hongyu Zhan <hongyu.zhan@gmail.com>
2023-07-20 16:43:55 -0700

    properly use assert

--- Practice-SortNumbers.py
+++ Practice-SortNumbers.py
@@ -10,9 +10,13 @@ def sort_and_remove_duplicates(input_list):
     return sorted_list
 
 def main():
-    # Input the two lists directly using assert
-    assert (list1 := "1 1 3 5")  # Modify the values in the list1 and list2 strings as needed
-    assert (list2 := "2 6 8")
+    # Input the two lists directly using assert with proper format
+    list1 = "1 1 3 5"  # Modify the values in the list1 and list2 strings as needed
+    list2 = "2 6 8"
+
+    # Ensure that the input lists contain only integers separated by spaces
+    assert all(num.isdigit() for num in list1.split()), "list1 should contain only integers separated by spaces"
+    assert all(num.isdigit() for num in list2.split()), "list2 should contain only integers separated by spaces"
 
     # Combine the two lists and remove duplicates
     combined_list = list1.split() + list2.split()
@@ -26,8 +30,7 @@ def main():
     assert len(set(sorted_list)) == len(sorted_list), f"Combined list contains duplicates: {combined_list}"
 
     # Assert the sorted list is correct
-    expected_result = sorted(list(set(sorted_list)))
-    assert sorted_list == expected_result, f"Sorting failed. Expected: {expected_result}, Got: {sorted_list}"
+    assert sorted_list == [1, 2, 3, 5, 6, 8], f"Sorting failed. Expected: [1, 2, 3, 5, 6, 8], Got: {sorted_list}"
 
 if __name__ == "__main__":
     main()
