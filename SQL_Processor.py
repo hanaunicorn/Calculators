faaa4a47c2cb9b963aedab8e706ee1ffc658cb5e
Hongyu Zhan <hongyu.zhan@gmail.com>
2023-08-02 09:22:49 -0700

    finishh

--- SQL_Processor.py
+++ SQL_Processor.py
@@ -1,50 +1,81 @@
+import re
+# re helps match strings and search for them
+
 # Store the table data as a dictionary of table names and their rows
 tables = {}
 
 def dojob(sql):
     global tables
 
-    # Split the SQL command into parts
-    parts = sql.strip().split()
+    # Regular expression patterns to match SQL commands
+    create_match = re.match(r'create table (\w+) \((\w+) int\);', sql, re.IGNORECASE)
+    insert_match = re.match(r'insert into (\w+) values\((\d+)\);', sql, re.IGNORECASE)
 
-    if parts[0].lower() == "create":
+    if create_match:
+        # If it's a "CREATE TABLE" command, extract table_name and column_name
+        table_name, column = create_match.groups()
         # Create an empty list for the table to store rows
-        tables[parts[2]] = []
+        tables.setdefault(table_name, [])
 
-    elif parts[0].lower() == "insert":
-        table = parts[2]
+    elif insert_match:
+        # If it's an "INSERT INTO" command, extract table_name and value
+        table_name, value = insert_match.groups()
         # Check if the table has reached its maximum capacity of 3 rows
-        if len(tables.get(table, [])) >= 3:
-            print("Table full")
+        if len(tables.get(table_name, [])) < 3:
+            # If not full, append the value as an integer to the table
+            tables.setdefault(table_name, []).append(int(value))
         else:
-            row = parts[-1].strip("();")
-            # If the table does not exist, create it with an empty list
-            tables.setdefault(table, []).append(row)
+            # If full, print a message indicating the table is full
+            print(f"Table {table_name} is full")
 
-    elif parts[0].lower() == "select":
-        # Print the rows joined as a string
-        print(', '.join(tables.get(parts[2], [])))
+    elif sql.strip().lower().startswith("select"):
+        # Matches words to commands
+        table_name = re.search(r'from (\w+);', sql, re.IGNORECASE).group(1)
+        # Get the rows from the table
+        rows = tables.get(table_name, [])
+        # Print the table name
+        print(f"Table {table_name}:")
+        # Convert the rows to strings and join them with commas
+        rows_str = ', '.join(map(str, rows))
+        # Print the rows
+        print(rows_str)
+        # Return the rows as a list of integers
+        return rows
 
     else:
+        # If the command is not recognized, raise an error
         raise ValueError("Invalid SQL command.")
 
 def main():
     try:
-        # Create table a and insert values
-        dojob("create table a (i int);")
-        dojob("insert into a values(10);")
-        dojob("insert into a values(2);")
-        dojob("select i from a;")  # Output: 10, 2
-        dojob("insert into a values(3);")
-        dojob("select i from a;")  # Output: 10, 2, 3
+        sql_commands = [
+            "create table a (i int);",
+            "insert into a values(10);",
+            "insert into a values(2);",
+            "insert into a values(3);",
+            "create table b (i int);",
+            "insert into b values(1);",
+            "insert into b values(4);",
+            "insert into b values(5);",
+            "select i from a;",
+            "select i from b;"
+        ]
 
-        # Create table b and insert values
-        dojob("create table b (i int);")
-        dojob("insert into b values(1);")
-        dojob("select i from b;")  # Output: 1
+        # Collect the values for each table
+        table_values = {}
+        for sql in sql_commands:
+            values = dojob(sql)
+            if values is not None:
+                table_name = re.search(r'from (\w+);', sql, re.IGNORECASE).group(1)
+                table_values[table_name] = values
+
+        # Print the collected values for each table
+        for table_name, values in table_values.items():
+            print(f"Values for Table {table_name}: {', '.join(map(str, values))}")
 
     except ValueError as e:
         print(f"Error: {str(e)}")
 
 if __name__ == "__main__":
     main()
+
